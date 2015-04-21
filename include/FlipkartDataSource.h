aaaca442ee3dffa0ba133d9f7682a01c22171a3b
vishal_salgotra <salgotra.vishal@gmail.com>
2015-04-22 01:18:35 +0530

    Added skelton for Remote Data Sources

---
+++ include/FlipkartDataSource.h
@@ -0,0 +1,21 @@
+#ifndef _FLIPKART_DATA_SOURCE_
+#define _FLIPKART_DATA_SOURCE_
+
+#include "RemoteDataSource.h"
+
+namespace Tc98
+{
+	namespace AffliateAnalytics
+	{
+		class FlipkartDataSource : public RemoteDataSource
+		{
+			FlipkartDataSource();
+			FlipkartDataSource(const std::string uri, const std::string relativePath, const std::unordered_map<std::string, std::string>::iterator it)
+			~FlipkartDataSource();
+
+			bool SendRequest();
+		};
+	}
+}
+
+#endif
